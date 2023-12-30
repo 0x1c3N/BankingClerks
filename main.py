0791004553b22d04230609e812569ba6b6149042
Furkan <furkanelcelik1@gmail.com>
2023-12-30 17:45:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,44 +4,40 @@ import datetime
 class Customer:
     def __init__(self, name, customer_type):
         self.name = name
-        self.wait_time = 0
         self.arriving_time = 0
         self.customer_type = customer_type
-        if customer_type == "commercial":
-            self.process_time = 10
-        elif customer_type == "casual":
-            self.process_time = 15
-        elif customer_type == "loan":
-            self.process_time = 20
+        self.process_time=0   
     
     def display_info(self):
         print(f"Customer Name: {self.name}")
         print(f"Customer Type: {self.customer_type}")
         print(f"Customer Process Time: {self.process_time}")
-        print(f"Customer Wait Time: {self.wait_time}")
-        print(f"Customer Arriving zaza Time: {self.arriving_time}")
         # Calculate the arriving time in "hh:mm" format
         formatted_time = datetime.datetime.strptime('09:00', '%H:%M') + datetime.timedelta(minutes=self.arriving_time)
         formatted_time = formatted_time.strftime('%H:%M')  # Format time as hh:mm
         print(f"Customer Arriving Time: {formatted_time}")
         print("")
+        
 
 def randomCustomer():
     customer_types = ["commercial", "casual", "loan"]
     customers = []
     r = random.randrange(20, 60)
-    for i in range(r):
+    for i in range(200):
         name = f"Customer{i+1}"
         customer_type = random.choice(customer_types)
-        wait_time = random.randint(60, 180)
-        arriving_time = random.randint(0, 180)  # Generate random arriving time between 0 and 180 minutes
+        process_time = random.randint(5, 15)
+        arriving_time = random.randint(0, 160)  # Generate random arriving time between 0 and 160 minutes
         customer = Customer(name=name, customer_type=customer_type)
-        customer.wait_time = wait_time
+        customer.process_time = process_time
         customer.arriving_time = arriving_time
         customers.append(customer)
-    return customers
+        # Sorting custumers 
+        customers.sort(key=lambda x: x.arriving_time)
+        return customers
 
 random_customers = randomCustomer()
+
 for customer in random_customers:
     print("-------------")
     customer.display_info()
