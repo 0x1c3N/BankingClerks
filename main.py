3af83a316c151b063d1f0490f4210a9acf30eb03
0x1c3N <bthnstars@gmail.com>
2024-01-14 16:41:49 +0300

    Fix Algorithm And Add Custom Wait Time

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from collections import defaultdict
 import random
 import datetime
 
-#maxWaitTime = 30
-
+maxWaitTimes = [60, 45, 60] #Default max waiting times for casual, commercial, loan
 
 class Customer:
     def __init__(self, name, customer_type):
@@ -12,7 +11,11 @@ class Customer:
         self.arriving_time = 0
         self.customer_type = customer_type
         self.process_time = 0
-        self.maxWaitTime = 60
+        self.maxWaitTime =maxWaitTimes[0]
+        if (customer_type == "commercial"):
+            self.maxWaitTime = maxWaitTimes[1]
+        elif (customer_type == "loan"):
+            self.maxWaitTime = maxWaitTimes[2]
         self.waitTime = -1
         self.serviceTime = -1
         self.isDone = False
@@ -34,17 +37,6 @@ class Customer:
         print(f"Customer Process Time: {self.process_time}")
         print(f"Served Clercks: {self.clerkIndex}\n\n")
 
-    def display_info(self):
-        print(f"\n\nCustomer Name: {self.name}")
-        print(f"Customer Type: {self.customer_type}")
-        print(f"Customer Process Time: {self.process_time}")
-        # Calculate the arriving time in "hh:mm" format
-        formatted_time = datetime.datetime.strptime('09:00', '%H:%M') + datetime.timedelta(minutes=self.arriving_time)
-        formatted_time = formatted_time.strftime('%H:%M')  # Format time as hh:mm
-        print(f"Customer Arriving Time: {formatted_time}")
-        print("")
-
-
 class Clerk:
     def __init__(self,index):
         self.index = index
@@ -57,34 +49,19 @@ class Clerk:
         self.currentCustomer = Customer("null", "null")
         self.availableTime = -1
 
-    def display_info(self):
-        print(f"Customer Name: {self.name}")
-        print(f"Customer Type: {self.customer_type}")
-        print(f"Customer Process Time: {self.process_time}")
-        # Calculate the arriving time in "hh:mm" format
-        formatted_time = datetime.datetime.strptime('09:00', '%H:%M') + datetime.timedelta(minutes=self.arriving_time)
-        formatted_time = formatted_time.strftime('%H:%M')  # Format time as hh:mm
-        print(f"Customer Arriving Time: {formatted_time}")
-        print("")
-
-
 def randomCustomer():
     customer_types = ["commercial", "casual", "loan"]
     customers = []
-    r = random.randrange(20, 60)
     for i in range(200):
         name = f"Customer{i + 1}"
         customer_type = random.choice(customer_types)
-        process_time = random.randint(5, 15)
-        arriving_time = random.randint(1, 100)  # Generate random arriving time between 0 and 160 minutes
-        customer = Customer(name=name, customer_type=customer_type)
-        customer.process_time = process_time
-        customer.arriving_time = arriving_time
+        customer = Customer(name=name, customer_type=random.choice(customer_types))
+        customer.process_time = random.randint(5, 15)
+        customer.arriving_time = random.randint(0, 100)  # Generate random arriving time between 0 and 100 minutes
         customers.append(customer)
         # Sorting custumers
     customers.sort(key=lambda x: x.arriving_time)
     return customers
-
 
 def separate_customers_by_type(customers):
     customer_types = defaultdict(list)
@@ -92,71 +69,6 @@ def separate_customers_by_type(customers):
     for customer in customers:
         customer_types[customer.customer_type].append(customer)
     return customer_types
-
-#after copmparing those 2 methods we will decide to use one of them
-"""def min_clerks_for_customer_type(customers, clerk_count, wait_time=0, memo={}):
-    if not customers:
-        return clerk_count
-
-    key = (len(customers), clerk_count, wait_time)
-    if key in memo:
-        return memo[key]
-    current_customer = customers[0]
-    remaining_customers = customers[1:]
-    if wait_time > 60:
-        memo[key] = min_clerks_for_customer_type(list(customers), clerk_count + 1, 0, memo)
-        return memo[key]
-    next_wait_time = wait_time + current_customer.process_time
-    memo[key] = min_clerks_for_customer_type(remaining_customers, clerk_count, next_wait_time, memo)
-    return memo[key]
-    
-    def min_clerk_c(customers, customer_type):
-    clerksIndex = 0
-    if customer_type == "casual":
-        clerksIndex = 1
-    elif customer_type == "loan":
-        clerksIndex = 2
-
-    global time
-    global totalTime
-    simulatingTime = 0;
-    while(simulatingTime<totalTime):
-        reset_customers(customers)
-        arrived_customer = []
-        for customer in customers:
-            if customer.arriving_time <= simulatingTime and customer.isDone == False:
-                arrived_customer.append(customer)
-        arrived_customer = (list(arrived_customer))
-        if(len(arrived_customer) > 0):
-            min_clerk(arrived_customer, clerksIndex, simulatingTime)
-        simulatingTime += 1
-
-
-def min_clerk(arrivedCustomer, clerkIndex, processTime):
-    global time
-    global totalTime
-    _arrivedCustomer = arrivedCustomer
-    lastCustomer = _arrivedCustomer[len(arrivedCustomer) - 1]
-    while lastCustomer.waitTime < maxWaitTime:
-        i = time
-        clerk = Clerk(processTime)
-        allClerks[clerkIndex].append(clerk)
-        lastCustomer.waitTime = -1
-        while i < totalTime:
-            for _clerk in allClerks[clerkIndex]:
-                if _clerk.availableTime <= i and _clerk.isBusy == True:
-                    _clerk.isBusy = False
-                if _clerk.isBusy == False:
-                    if len(arrivedCustomer) == 0:
-                        break
-                    _customer = arrivedCustomer.pop(0)
-                    _clerk.currentCostumer = _customer
-                    _clerk.isBusy = True
-                    _clerk.availableTime = i + processTime
-                    _customer.waitTime = i - _customer.arriving_time
-            i += 1
-    """
-
 
 clerksCommercial = []
 clerksCasual = []
@@ -169,11 +81,9 @@ def reset_customers(customerList):
     for customer in customerList:
         customer.reset()
 
-
 def reset_clerks(clerkList):
     for clerk in clerkList:
         clerk.reset()
-
 
 
 def min_clerk_algo(customers, customer_type):
@@ -199,14 +109,15 @@ def min_clerk_algo(customers, customer_type):
             for _clerk in allClerks[clerksIndex]:
                 if len(waitingCustomer)>0:
                     if _clerk.isBusy and _clerk.availableTime<=time:
-                        _clerk.isBusy = False;
+                        _clerk.isBusy = False
                         _clerk.availableTime = -1
-                        _clerk.currentCostumer.isDone = True;
+                        _clerk.currentCostumer.isDone = True
                     if not _clerk.isBusy:
                         _clerk.isBusy = True
                         _clerk.currentCostumer = waitingCustomer[0]
                         _clerk.availableTime = _clerk.currentCostumer.process_time + time
                         _clerk.currentCostumer.clerkIndex = _clerk.index
+                        _clerk.currentCostumer.isDone = True
                         waitingCustomer.remove(_clerk.currentCostumer)
                         _clerk.currentCostumer.waitTime = time -  _clerk.currentCostumer.arriving_time
                         _clerk.currentCostumer.serviceTime = time
@@ -216,53 +127,38 @@ def min_clerk_algo(customers, customer_type):
                 clerkFlag = True
                 break
 
-    reset_clerks(allClerks[clerksIndex])
-    reset_customers(customers)
-    waitingCustomer = []
-    timeFlag = False
-    for time in range(totalTime + 1):
-        if timeFlag:
-            break
-        for customer in customers:
-            if (customer.arriving_time <= time and customer.isDone == False and customer not in waitingCustomer):
-                waitingCustomer.append(customer)
-        for _clerk in allClerks[clerksIndex]:
-            if len(waitingCustomer) > 0:
-                if _clerk.isBusy and _clerk.availableTime <= time:
-                    _clerk.isBusy = False;
-                    _clerk.availableTime = -1
-                    _clerk.currentCostumer.isDone = True;
-                if not _clerk.isBusy:
-                    _clerk.isBusy = True
-                    _clerk.currentCostumer = waitingCustomer[0]
-                    _clerk.availableTime = _clerk.currentCostumer.process_time + time
-                    _clerk.currentCostumer.clerkIndex = _clerk.index
-                    waitingCustomer.remove(_clerk.currentCostumer)
-                    _clerk.currentCostumer.waitTime = time - _clerk.currentCostumer.arriving_time
-                    _clerk.currentCostumer.serviceTime = time
-                    if _clerk.currentCostumer.waitTime > _clerk.currentCostumer.maxWaitTime:
-                        timeFlag = True
-
-
     return (allClerks[clerksIndex])
 
 def printCustomers(customers):
     for customer in customers:
-        customer.display();
+        customer.display()
 
+def main():
 
+    # waitTimeChoice = input("Dou you want to set custom waiting times for Customer types? (Y,N) = ")
 
+    # if (waitTimeChoice.upper() == "Y"):
+    #     maxWaitTimeCasual = int(input("What do you want to set max wait time for Casual customer type: "))
+    #     maxWaitTimeCommercial = int(input("What do you want to set max wait time for Commercial customer type: "))
+    #     maxWaitTimeLoan = int(input("What do you want to set max wait time for Loan customer type: "))
 
+    #     maxWaitTimes[0] = maxWaitTimeCasual
+    #     maxWaitTimes[1] = maxWaitTimeCommercial
+    #     maxWaitTimes[2] = maxWaitTimeLoan
 
-random_customers = randomCustomer()
+    # else:
+    #     if (waitTimeChoice.upper() != "N"):
+    #         print("Wrong input!!, you should give only 'Y','y' or 'N','n'")
+    #         exit(0)
 
-separated_customers = separate_customers_by_type(random_customers)
+    random_customers = randomCustomer()
+    separated_customers = separate_customers_by_type(random_customers)
 
-p = 0
+    for customer_type, customer_list in separated_customers.items():
+        clercks =  min_clerk_algo(list(customer_list), customer_type)
+        print( f"Customer Count:{len(customer_list)}", f"\nClerk Count:{len(clercks)}", f"\nType:{customer_type} \n\n")
+        printCustomers(customer_list)
 
-for customer_type, customer_list in separated_customers.items():
-    time = 45
-    clercks =  min_clerk_algo(list(customer_list), customer_type)
-    print( f"Customer Count:{len(customer_list)}", f"\nClerk Count:{len(clercks)}", f"\nType:{customer_type} \n\n")
-    printCustomers(customer_list)
-    p += 1
+if __name__ == "__main__":
+
+    main()
